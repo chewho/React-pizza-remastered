286d96fff5f443c9be03cf7ee7ddcb6be40a0a2a
chewho <molocrips@gmail.com>
2023-09-22 17:57:58 +0200

    feat: add filter by category, sorting using useState in parent component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 
-const Categories = () => {
-  const [activeIndex, setActiveIndex] = React.useState(0);
-
+const Categories = ({ value, onChangeCategory }) => {
   const categories = ["Все", "Мясные", "Вегетарианские", "Гриль", "Острые", "Закрытые"];
+
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, i) => (
-          <li
-            key={i}
-            onClick={() => setActiveIndex(i)}
-            className={activeIndex === i ? "active" : ""}
-          >
-            {value}
+        {categories.map((categoryName, i) => (
+          <li key={i} onClick={() => onChangeCategory(i)} className={value === i ? "active" : ""}>
+            {categoryName}
           </li>
         ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,27 +8,37 @@ import Skeleton from "../components/PizzaBlock/Skeleton";
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
 
   React.useEffect(() => {
-    fetch("https://64b926cd79b7c9def6c0a704.mockapi.io/items")
+    setIsLoading(true);
+    fetch(
+      `https://64b926cd79b7c9def6c0a704.mockapi.io/items?${
+        categoryId > 0 ? `category=${categoryId}` : ""
+      }&sortBy=${sortType.sortProperty}&order=desc`
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
