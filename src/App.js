ca94b23b63938bf85c73496579dae88a16c56409
chewho <molocrips@gmail.com>
2023-10-02 17:33:43 +0200

    feat: add remove icon for Input component

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ import NotFound from "./pages/NotFound";
 import "./scss/app.scss";
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState("");
+
   return (
     <div className="wrapper">
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
