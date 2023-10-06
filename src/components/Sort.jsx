2c420c1d5500d9dfa875b796cc23e3d832309e5c
chewho <molocrips@gmail.com>
2023-10-06 16:08:28 +0200

    feat: add removing sort popup when click outside this popup

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useSelector, useDispatch } from "react-redux";
 import { setSort } from "../redux/slices/filterSlice";
+import { click } from "@testing-library/user-event/dist/click";
 
 export const list = [
   { name: "популярности", sortProperty: "rating" },
@@ -12,6 +13,7 @@ export const list = [
 const Sort = () => {
   const dispatch = useDispatch();
   const sort = useSelector((state) => state.filter.sort);
+  const sortRef = React.useRef();
 
   const [open, setOpen] = React.useState(false);
 
@@ -20,8 +22,23 @@ const Sort = () => {
     setOpen(false);
   };
 
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      const isOutside = !sortRef.current.contains(event.target);
+      if (isOutside) {
+        setOpen(false);
+      }
+    };
+
+    document.body.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
