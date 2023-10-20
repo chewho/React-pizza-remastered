545ab4558cc2f06238d1de12f062c87df03734c9
chewho <molocrips@gmail.com>
2023-10-20 18:03:45 +0200

    feat: fix bug with totalPrice in cart, remove search field when cart is open

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -45,9 +45,13 @@ export const cartSlice = createSlice({
       if (findItem) {
         findItem.count--;
       }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum;
+      }, 0);
     },
     removeItem(state, action: PayloadAction<string>) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
+      state.totalPrice = 0;
     },
     clearItems(state) {
       state.items = [];
